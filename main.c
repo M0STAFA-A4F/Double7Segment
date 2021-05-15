562391764d88d33b09aa5abc19338c3c721d3457
Mostafa Ashraf <64167655+M0STAFA-A4F@users.noreply.github.com>
2021-05-15 13:18:58 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <avr/io.h>
+#include <avr/delay.h>
+#include "STD_TYPES.h"
+
+#define ones	0x02
+#define tens	0x01
+
+
+int main(void)
+{
+	u8 numbers[10] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
+
+	DDRB = 0xff;
+	DDRA = 0xff;
+
+	PORTA = 0x03;
+
+	while(1)
+	{
+		for(u8 i=0; i<10; i++)
+		{
+			for(u8 j=0; j<10; j++)
+			{
+				u8 time = 50;
+				while(time--)
+				{
+					PORTB = numbers[i];
+					PORTA = tens;
+					_delay_ms(10);
+					PORTB = numbers[j];
+					PORTA = ones;
+					_delay_ms(10);
+				}
+			}
+		}
+
+		for(s8 i=9; i>=0; i--)
+		{
+			for(s8 j=9-(i==9); j>=0; j--)
+			{
+				u8 time = 50;
+				while(time--)
+				{
+					PORTB = numbers[i];
+					PORTA = tens;
+					_delay_ms(10);
+					PORTB = numbers[j];
+					PORTA = ones;
+					_delay_ms(10);
+				}
+			}
+		}
+	}
+
+	return 0;
+}
